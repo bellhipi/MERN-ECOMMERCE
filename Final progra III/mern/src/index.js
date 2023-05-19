429602a37a8cf18f09793cf7a815934d98fdb888
bellhipi <91761857+bellhipi@users.noreply.github.com>
2023-05-19 18:52:35 -0300

    initial commit
    Hasta el minuto 22 del tuto

---
+++ Final progra III/mern/src/index.js
@@ -0,0 +1,16 @@
+ const express = require('express');
+ const app = express(); // servidor
+
+ //Settings
+
+ //Midddlewares
+
+ //Routes
+ 
+ //Static files
+
+ //Ststing the server
+ // inicializa el servidor y le pedimos que la funcion  nos muestre el mnsj
+ app.listen(3000, () =>{
+    console.log('server on port 4000');
+ });
