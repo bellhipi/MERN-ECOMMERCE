b83ac5c7d014c8d09bfb9f5c07cd4679b2fe9e3d
bellhipi <91761857+bellhipi@users.noreply.github.com>
2023-05-31 18:40:14 -0300

    modelando la bd

--- Final progra III/mern/src/models/task.js
+++ Final progra III/mern/src/models/task.js
@@ -1,3 +1,14 @@
 const mongoose = require('mongoose');
+const {Schema} = mongoose;
+//mongoose.connect()
 
-mongoose.connect()
+//defino el esquema de una tarea
+// campo, tipo de dato, required (si es obligatorio)
+//como lo guardo en una constante despues lo puedo reutilizar
+const TaskSchema = new Schema({
+    title: { type: String, required: true},
+    description: { type: String, required: true}
+});
+
+//exporto el modelo de la base de datos
+module.exports = mongoose.model('Task', TaskSchema);
--- Final progra III/mern/src/routes/tasks.routes.js
+++ Final progra III/mern/src/routes/tasks.routes.js
@@ -1,8 +1,21 @@
 const express = require('express');
 const router = express.Router();
 
+//guardo mi modelo de bd en la constante Task
+//con Task voy a poder hacer consultas a la bd
+const Task = require('../models/task');
+
 //aca defino mi rsapi
 router.get('/', (req, res) => { //cuando llegue una peticion
+    //consulto a la bd
+    //el find busca los documentos (puedo obtener los errores, o los documentos)
+    //en el tutorial era así:
+    //Task.find(function (err, tasks){
+    //pero me dio err
+    Task.find().then(function (err, tasks){
+        //muestro las tareas que tengo
+        console.log(tasks);
+    });
     //res.send('Holis'); //respondo con holis
     res.json({  //en lugar de texto paso un json
         status: 'API Works!'
