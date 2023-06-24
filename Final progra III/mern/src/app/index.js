dc243d93e519cbf0248e50b301a87814db36f031
bellhipi <91761857+bellhipi@users.noreply.github.com>
2023-06-24 20:18:03 -0300

    react + error
    hasta acá llegué estoy haciendo la traducción de js + html a react pero cuando quiero ejecutar la parte de la config del webpack obtengo este error:
    node:internal/process/promises:279
                triggerUncaughtException(err, true /* fromPromise */);
                ^

--- Final progra III/mern/src/app/index.js
+++ Final progra III/mern/src/app/index.js
@@ -1,1 +1,20 @@
-alert ('react va acá');
+//alert ('react va acá');
+
+//Importo los módulos de react
+import React, { Component } from 'react'; //importo el componenete
+//import ReactDOM from 'react-dom'; en lugar de llamar a ReactDom
+import { render } from 'react-dom'; //importo render directamente
+//esta facilidad se llama restrucsturing y se puede hacer gracias a las nuevas versiones de js
+
+class App extends Component{
+    //utilizo el metodo render
+    render() {
+        return(
+            <h1>Holita</h1>
+        )
+    }
+}
+
+//voy a renderizar este componente
+//es decir que voy a montar el h1 Holita en donde está el id app
+render (<App/>,document.getElementById('app'))
--- Final progra III/mern/webpack.config.js
+++ Final progra III/mern/webpack.config.js
@@ -5,7 +5,23 @@ module.exports ={
     entry: './src/app/index.js',
     //output es la salida qque es el código convertido
     output: {
-        path: __dirname + '/srcpublic',
-        filename: 'bundle.j s'
+        //_dirname es una constante de node
+        path: __dirname + '/src/public',
+        filename: 'bundle.js'
+    },
+    //module es un objeto que tiene rules que es un arreglo formado por multiples objetos
+    //cada objeto es una configuración adicional a webpack
+    module: {
+        rules: [
+            {
+                //le pido que use babel loader (el modulo que instale)
+                use: 'babel-loader',
+                //le pido que teste todos los archivos que terminen .js
+                //  /\.js$/ esto es una expresión regular
+                test: /\.js$/,
+                //pero no quiero que tome los archivos que están en node_modules
+                exclude: /node_modules/
+            }
+        ]
     }
 };
