5cdab044ac75d6260f4d711f97096da9e560da86
bellhipi <91761857+bellhipi@users.noreply.github.com>
2023-08-09 16:49:47 -0300

    estoy trabajando con el framework

---
+++ Final progra III/mern/src/app/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'; //importo el componenete
+
+class App extends Component{
+    //utilizo el metodo render
+    render() {
+        return(
+            <div>
+                <h1>Testing</h1>
+            </div>
+        )
+    }
+}
+
+export default App; //exporto el componente
--- Final progra III/mern/src/app/index.js
+++ Final progra III/mern/src/app/index.js
@@ -1,20 +1,24 @@
 //alert ('react va acá');
 
 //Importo los módulos de react
-import React, { Component } from 'react'; //importo el componenete
+//import React, { Component } from 'react'; //importo el componenete
+import React from 'react';
 //import ReactDOM from 'react-dom'; en lugar de llamar a ReactDom
 import { render } from 'react-dom'; //importo render directamente
 //esta facilidad se llama restrucsturing y se puede hacer gracias a las nuevas versiones de js
 
-class App extends Component{
+//class App extends Component{
     //utilizo el metodo render
-    render() {
-        return(
-            <h1>Holita</h1>
-        )
-    }
-}
+//    render() {
+//        return(
+//            <h1>Chausito</h1>
+//        )
+//    }
+//}
+//lo convierto en un componenete
+//importo el componente que cree en App.js
+import App from './App';
 
 //voy a renderizar este componente
 //es decir que voy a montar el h1 Holita en donde está el id app
-render (<App/>,document.getElementById('app'))
+render (<App/>,document.getElementById('app'));
